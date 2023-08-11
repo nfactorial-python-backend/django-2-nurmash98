ee24a3c49ce64a69856e3b97f0ed3098e8edd574
Nurmash Omarbek <nurmash98@github.com>
2023-08-11 18:58:17 +0600

    added: get comments, add comment

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:news_id>/', views.get_news, name='get_news'),
     path('/add_news', views.add_news, name='add_news'),
+   
 ]
--- news/views.py
+++ news/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import News
+from .models import News, Comment
 
 
 
 def index(request):
     news = News.objects.all().order_by('-created_at')
+
     return render(request, 'news/index.html', {'list_news': news})
 
 def get_news(request, news_id):
     news = get_object_or_404(News, id=news_id)
-    return render(request, 'news/detail.html', {'news': news})
+    comments = news.comments.all().order_by('-created_at')
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        comment = Comment(content=content, news = news)
+        comment.save()
+        return redirect('news:get_news', news_id=news_id)
+    return render(request, 'news/detail.html', {'news': news, 'comments': comments})
 
 def add_news(request):
     if request.method == 'POST':
